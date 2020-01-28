6b4a1d43e08789e2a6cc5763b3f0471317df39eb
f2114198@163.com <f2114198@163.com>
2020-01-28 20:43:58 +0800

    add cat controller

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
@@ -0,0 +1,79 @@
+package com.imooc.controller;
+
+import com.imooc.enums.YesOrNo;
+import com.imooc.pojo.Carousel;
+import com.imooc.pojo.Category;
+import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
+import com.imooc.pojo.vo.CategoryVO;
+import com.imooc.service.CarouselService;
+import com.imooc.service.CategoryService;
+import com.imooc.service.UserService;
+import com.imooc.utils.CookieUtils;
+import com.imooc.utils.IMOOCJSONResult;
+import com.imooc.utils.JsonUtils;
+import com.imooc.utils.MD5Utils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Api(value = "首页", tags = {"首页展示相关接口"})
+@RestController
+@RequestMapping("index")
+public class IndexController {
+
+    final static Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @Autowired
+    public CarouselService carouselService;
+
+    @Autowired
+    public CategoryService categoryService;
+
+    @ApiOperation(value = "获取首页轮播图列表", notes = "获取首页轮播图列表", httpMethod = "GET")
+    @GetMapping("/carousel")
+    public IMOOCJSONResult carousel(){
+
+        List<Carousel> list = carouselService.queryAll(YesOrNo.YES.type);
+
+        return IMOOCJSONResult.ok(list);
+    }
+
+    /**
+     * 首页分类展示需求
+     * 1. 第一次刷新主页大分类
+     * 2, 如果移动到大分类中的一个,则加载子分类
+     */
+    @ApiOperation(value = "获取商品分类(一级分类)", notes = "获取商品分类(一级分类)", httpMethod = "GET")
+    @GetMapping("/cats")
+    public IMOOCJSONResult cats(){
+
+        List<Category> list = categoryService.queryAllRootLevelCat();
+
+        return IMOOCJSONResult.ok(list);
+    }
+
+    @ApiOperation(value = "获取商品子分类", notes = "获取商品子分类", httpMethod = "GET")
+    @GetMapping("/subCat/{rootCatId}")
+    public IMOOCJSONResult subCat(
+            @ApiParam(name = "rootCatId", value = "一级分类id")
+            @PathVariable Integer rootCatId){
+
+        if(rootCatId == null){
+            return IMOOCJSONResult.errorMsg("");
+        }
+
+        List<CategoryVO> list = categoryService.getSubCatList(rootCatId);
+
+        return IMOOCJSONResult.ok(list);
+    }
+}
