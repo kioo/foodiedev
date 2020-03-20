e53dbb9896c6de7edb14e6bf678de0d144c6273b
kioo <f2114198@163.com>
2020-03-20 17:25:40 +0800

    add address modual

--- foodie-dev-service/src/main/java/com/imooc/service/AddressService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/AddressService.java
@@ -2,6 +2,7 @@ package com.imooc.service;
 
 import com.imooc.pojo.Carousel;
 import com.imooc.pojo.UserAddress;
+import com.imooc.pojo.bo.AddressBO;
 
 import java.util.List;
 
@@ -11,4 +12,30 @@ public interface AddressService {
      * 根据用户id 查询用户列表
      */
     public List<UserAddress> queryAll(String userId);
+
+    /**
+     * 用户新增地址
+     * @param addressBO
+     */
+    public void addNewUserAddress(AddressBO addressBO);
+
+    /**
+     * 更新用户地址
+     * @param addressBO
+     */
+    public void updateUserAddress(AddressBO addressBO);
+
+    /**
+     * 根据用户id和地址id删除地址信息
+     * @param userId
+     * @param addressId
+     */
+    public void deleteUserAddress(String userId, String addressId);
+
+    /**
+     * 根据用户id和地址id 设置默认地址信息
+     * @param userId
+     * @param addressId
+     */
+    public void updateUserAddressToBeDefault(String userId, String addressId);
 }
