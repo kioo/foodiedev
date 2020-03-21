59f40deb972aaa9cda1e2fa92c07dc836c8f90df
kioo <f2114198@163.com>
2020-03-21 22:09:16 +0800

    add order model

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
@@ -0,0 +1,46 @@
+package com.imooc.controller;
+
+import com.imooc.pojo.UserAddress;
+import com.imooc.pojo.bo.AddressBO;
+import com.imooc.pojo.bo.SubmitOrderBO;
+import com.imooc.service.AddressService;
+import com.imooc.utils.IMOOCJSONResult;
+import com.imooc.utils.MobileEmailUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(value = "订单相关", tags = {"地址相关接口api接口"})
+@RestController
+@RequestMapping("address")
+public class OrdersController {
+
+    final static Logger logger = LoggerFactory.getLogger(OrdersController.class);
+
+    @Autowired
+    private AddressService addressService;
+
+    @ApiOperation(value = "用户下单", notes = "用户下单", httpMethod = "POST")
+    @PostMapping("/create")
+    public IMOOCJSONResult create(@RequestBody SubmitOrderBO submitOrderBO){
+
+        System.out.println(submitOrderBO.toString());
+
+        // 1. 创建订单
+        // 2. 创建订单后，移除购物车中已结算的商品
+        // 3. 向支付中心发送当前订单，用于保存中心订单
+
+
+        return IMOOCJSONResult.ok();
+    }
+
+
+
+
+}
